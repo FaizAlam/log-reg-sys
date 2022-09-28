6d55886cec2a0dcbaf35bc8d732b6101af7fe1c4
FaizAlam <mohdfaizalam53@gmail.com>
2022-09-29 01:24:44 +0530

    changes

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const nodemailer = require("nodemailer")
 const {google} = require("googleapis")
 
 const app = express()
-const port = process.env.PORT ||5000
+const port = process.env.PORT || 5000
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(session({
